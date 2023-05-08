4a7378818a07efd9d315082f32bdaa0ae7a6a11b
Aboudoc <aboutika.reda@gmail.com>
2023-05-08 16:40:07 +0200

    deal and hoax

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -3,19 +3,23 @@ pragma solidity ^0.8.18;
 contract Wallet {
     address payable public owner;
 
-    constructor() {
+    event Deposit(address account, uint256 amount);
+
+    constructor() payable {
         owner = payable(msg.sender);
     }
 
-    receive() external payable {}
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function withdraw(uint256 _amount) external {
-        require(msg.sender == owner, "caller is not the owner");
+        require(msg.sender == owner, "caller is not owner");
         payable(msg.sender).transfer(_amount);
     }
 
     function setOwner(address _owner) external {
-        require(msg.sender == owner, "caller is not the owner");
+        require(msg.sender == owner, "caller is not owner");
         owner = payable(_owner);
     }
 }
---
+++ test/Wallet.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {Wallet} from "../src/Wallet.sol";
+
+contract WalletTest is Test {
+    Wallet public wallet;
+
+    function setUp() public {
+        wallet = new Wallet();
+    }
+
+    function _send(uint256 amount) private {
+        (bool success, ) = address(wallet).call{value: amount}("");
+        require(success, "fail");
+    }
+
+    function testEthBalance() public {
+        console.log("ETH balance", address(this).balance / 1e18);
+    }
+
+    function testSendEth() public {
+        uint bal = address(wallet).balance;
+
+        deal(address(1), 100);
+        assertEq(address(1).balance, 100);
+
+        deal(address(1), 10);
+        assertEq(address(1).balance, 10);
+
+        deal(address(1), 123);
+        vm.prank(address(1));
+        _send(123);
+
+        hoax(address(1), 456);
+        _send(456);
+        assertEq(address(wallet).balance, bal + 456 + 123);
+    }
+}
