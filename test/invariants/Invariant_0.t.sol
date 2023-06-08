b5b5a0877180982523f095ba6cdf556982d0491c
Aboudoc <aboutika.reda@gmail.com>
2023-06-08 18:38:06 +0200

    invariant_test

---
+++ test/invariants/Invariant_0.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract IntroInvariant {
+    bool public flag;
+
+    function func_1() external {}
+    function func_2() external {}
+    function func_3() external {}
+    function func_4() external {}
+    function func_5() external {
+        flag = true;
+    }
+}
+
+import "forge-std/Test.sol";
+
+contract IntroInvariantTest is Test {
+    IntroInvariant private target;
+
+    function setUp() public {
+        target = new IntroInvariant();
+    }
+
+    // Failing invariant test 
+    function invariant_flag_is_always_false() public {
+        assertEq(target.flag(), false);
+    }
+}
+    
---
+++ test/invariants/Invariant_1.t.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {WETH} from "../../src/Weth.sol";
+
+// Note: open testing - randomly call all public functions 
+/*
+    (runs: 256, calls: 3840, reverts: 2228)
+    2228 reverts out of 3840 calls
+ */
+contract WETH_Open_Invariant_Tests is Test {
+    WETH private weth;
+
+    function setUp() public {
+        weth = new WETH();
+    }
+
+    function invariant_totalSupply_is_always_zero() public {
+        assertEq(weth.totalSupply(), 0);
+    }
+}
